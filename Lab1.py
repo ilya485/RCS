b50dfc52e76d825083ff03f8bbe02e61045d444f
ilya485 <56791905+ilya485@users.noreply.github.com>
2021-03-11 19:10:57 +0200

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,70 +1,55 @@
 # Лабораторна робота №1 на тему "Обчислення показників надійності за даними випробувань"
 # Виконав студент групи ІО-71 Кизима І.Л, варіант №7
 
-time_table = [577, 566, 251, 868, 2525, 26, 1040, 580, 57,
-376, 906, 304, 1407, 134, 62, 230, 531, 653,
-2143, 515, 701, 88, 762, 337, 278, 87, 85,
-10, 484, 23, 454, 61, 508, 15, 469, 309, 434,
-2323, 219, 120, 66, 212, 220, 498, 522, 267,
-820, 648, 18, 417, 519, 34, 662, 756, 461,
-221, 291, 224, 234, 365, 416, 114, 448, 395,
-71, 4, 44, 462, 537, 226, 125, 157, 18, 216,
-844, 438, 730, 274, 395, 40, 481, 118, 14,
-1297, 102, 249, 34, 758, 611, 159, 1115,
-353, 117, 15, 158, 259, 489, 38, 1416, 2344]
+T = [577, 566, 251, 868, 2525, 26, 1040, 580, 57,
+     376, 906, 304, 1407, 134, 62, 230, 531, 653,
+     2143, 515, 701, 88, 762, 337, 278, 87, 85,
+     10, 484, 23, 454, 61, 508, 15, 469, 309, 434,
+     2323, 219, 120, 66, 212, 220, 498, 522, 267,
+     820, 648, 18, 417, 519, 34, 662, 756, 461,
+     221, 291, 224, 234, 365, 416, 114, 448, 395,
+     71, 4, 44, 462, 537, 226, 125, 157, 18, 216,
+     844, 438, 730, 274, 395, 40, 481, 118, 14,
+     1297, 102, 249, 34, 758, 611, 159, 1115,
+     353, 117, 15, 158, 259, 489, 38, 1416, 2344]
 
 gamma = 0.91
 T_infail = 2245
-T_fail = 342
+I_fail = 342
 
-time_table = sorted(time_table)
-h = 0
-ten_intervals = []
-f_list = []
-P_list = []
+print("Середній наробіток: ", sum(T)/len(T))
 
-def get_T(gamma):
-    global h, f_list, ten_intervals, P_list
-    h = (time_table[-1] - time_table[0]) / 10
+T = sorted(T)
+#print(T)
+T_avg = sum(T) / len(T)
+h = (T[-1] - T[0]) / 10
+#print(h)
 
-    for i in range(10):
-        ten_intervals.append([a for a in time_table if (i * h <= a <= (i+1) * h)])
+intervals = [[a for a in T if (i * h <= a <= (i + 1) * h)] for i in range(10)]
+#for i in range(len(intervals)):
+#    print(i * h, " - ", h * (i + 1), intervals[i])
 
-    f_list = [len(interval) / (len(time_table) * h) for interval in ten_intervals]
-    area_sum = 1
+f = [len(i) / (len(T) * h) for i in intervals]
+#print("\nf list", f)
 
-    for i in range(10):
-        P_list.append(area_sum)
-        area_sum -= f_list[i] * h
+P = []
+s = 1
+for i in range(10):
+    P.append(s)
+    s -= f[i] * h
+#print("P ", P)
 
-    p_less = max([p for p in P_list if p < gamma])
-    p_more = min([p for p in P_list if p > gamma])
+P1 = max([p for p in P if p < gamma])
+P2 = min([p for p in P if p > gamma])
+index = P.index(P1)
+d = (P1 - gamma) / (P1 - P2)
+T_gamma = (h * index) - (h * d)
+print("γ-відсотковий наробіток на відмову Tγ: ", T_gamma)
 
-    index_more = P_list.index(p_more)
-    index_less = P_list.index(p_less)
+a = int(T_infail // h)
+Chance = 1 - sum([f[i] * h for i in range(a)]) - f[a] * (T_infail % h)
+print("Ймовірність безвідмовної роботи: ", Chance)
 
-
-    d = (p_less - gamma) / (p_less - p_more)
-    T = (h * index_less) - (h * d)
-    return T
-
-
-def time_unfail(time):
-    Sum = 1
-    whole_intervals = int(time // h)
-    for i in range(whole_intervals):
-        Sum -= f_list[i] * h
-    Sum -= f_list[whole_intervals] * (time % h)
-    return Sum
-
-
-def lamb(time):
-    f = f_list[int(time // h)]
-    p = time_unfail(time)
-    return f / p
-
-print("Середній наробіток до відмови Tср:", sum(time_table) / len(time_table))
-print("γ-відсотковий наробіток на відмову Tγ при γ = ",gamma,":", get_T(gamma))
-print("ймовірність безвідмовної роботи на час", T_infail, "годин:", time_unfail(T_infail))
-print("інтенсивність відмов на час ", T_fail, " годин:",  lamb(T_fail))
-
+a = int(I_fail // h)
+Intensivnost = f[int(I_fail // h)] / (1 - sum([f[i] * h for i in range(a)]) - f[a] * (I_fail % h))
+print("Інтенсивність відмов: ", Intensivnost)
